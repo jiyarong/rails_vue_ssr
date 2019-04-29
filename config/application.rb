2f5a7747c6f4adb8856d31713a0722ddcda7a394
纪亚荣 <jiyr@wfl-ischool.cn>
2019-04-29 09:50:22 +0800

    默认时区改为北京

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module RailsVueSsr
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.action_controller.perform_caching
+    config.time_zone = 'Beijing'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
